7e3e0c0d3ef3fbc0639556d31fa9e6cc825e11d5
s50984 <s50984@bht-berlin.de>
2023-07-31 23:37:39 +0200

    Host chat

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import socket, threading, time
-
+import socket, threading, time, os, sys
 from psutil import process_iter
 from signal import SIGTERM 
 
-from multiprocessing import Pipe, Process 
 
 class TCPconnection:
     def __init__(self):
@@ -14,30 +11,34 @@ class TCPconnection:
         self.s.bind(("", 50000)) 
         self.s.listen(1)
         self.kill = False
+        self.window = None
+
+
+    def setobserver(self, observer):
+        self.observer = observer
         
+    def startlistening(self):
         x = threading.Thread(target=self.listener, args=(), daemon=True)
         x.start()
-        print("Hier")
-        time.sleep(10)
-        self.kill = True
-
-    
-        
-        
+             
     def listener(self):
         try: 
             while True: 
-                komm, addr = self.s.accept()
+                self.komm, self.addr = self.s.accept()
                 while True: 
-                    data = komm.recv(1024) 
-                    if self.kill == True or data.decode() == "exit":
-                        self.s.close()
+                    data = self.komm.recv(1024) 
+                    if self.kill == True or data.decode() == "exitt":
                         break
-                    print("[{}] {}".format(addr[0], data.decode())) 
-                    nachricht = input("Antwort: ")
-                    komm.send(nachricht.encode()) 
+                    if len(data.decode()) > 1:
+                        print("[{}] {}".format(self.addr[0], data.decode()))
+                        self.window.appendMessage(data.decode())
+                        #self.observer.setmessage(data.decode())
         finally: 
+            print("Server closed.")
             self.s.close()
+            
+    def sendmessage(self, message):
+        self.komm.sendall(bytes(message,'utf-8'))
             
     @staticmethod
     def portcleaner():
@@ -45,68 +46,23 @@ class TCPconnection:
             for conns in proc.connections(kind='inet'):
                 if conns.laddr.port == 50000:
                     proc.send_signal(SIGTERM) 
-
-y = TCPconnection()
-#TCPconnection.portcleaner()
-
-
-# find port process
-# netstat -ano | findstr :50000
-# kill port process 
-=======
-import socket, threading, time
-
-from psutil import process_iter
-from signal import SIGTERM 
-
-from multiprocessing import Pipe, Process 
-
-class TCPconnection:
-    def __init__(self):
-        print("Server hört auf: "+socket.gethostbyname(socket.gethostname()))
-        
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind(("", 50000)) 
-        self.s.listen(1)
-        self.kill = False
-        
-        x = threading.Thread(target=self.listener, args=(), daemon=True)
-        x.start()
-        print("Hier")
-        time.sleep(10)
-        self.kill = True
-
+                    
+    def getipaddr(self):
+        return socket.gethostbyname(socket.gethostname())
     
-        
-        
-    def listener(self):
-        try: 
-            while True: 
-                komm, addr = self.s.accept()
-                while True: 
-                    data = komm.recv(1024) 
-                    if self.kill == True or data.decode() == "exit":
-                        self.s.close()
-                        break
-                    print("[{}] {}".format(addr[0], data.decode())) 
-                    nachricht = input("Antwort: ")
-                    komm.send(nachricht.encode()) 
-        finally: 
-            self.s.close()
-            
-    @staticmethod
-    def portcleaner():
-        for proc in process_iter():
-            for conns in proc.connections(kind='inet'):
-                if conns.laddr.port == 50000:
-                    proc.send_signal(SIGTERM) 
+    def setWindow(self, win):
+        self.window = win
 
-y = TCPconnection()
+if __name__ == "main":
+    y = TCPconnection()
+    y.startlistening()
+
+
+
 #TCPconnection.portcleaner()
 
 
 # find port process
 # netstat -ano | findstr :50000
 # kill port process 
->>>>>>> bf6d9c38a44edca4613c01f8cd8294c0c185e8a3
 # taskkill /PID <PID> /F
